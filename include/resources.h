2637d457212e11e8037a245fb63c78bad59b48e0
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-27 10:30:35 -0400

    feat: Add `Resources` class

---
+++ include/resources.h
@@ -0,0 +1,26 @@
+/*
+ * resources.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+class Resources
+{
+	public:
+		Resources();
+		virtual ~Resources();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* RESOURCES_H */ 
