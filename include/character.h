2d2effca681e78da90cb7d3195022785ed692cac
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-27 10:27:10 -0400

    feat: Add `Character` class

---
+++ include/character.h
@@ -0,0 +1,26 @@
+/*
+ * character.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+class Character
+{
+	public:
+		Character();
+		virtual ~Character();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* CHARACTER_H */ 
