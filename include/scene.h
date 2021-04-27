4d4e73fb7080b8535d18e2ed90d7daec1a4c1b71
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-27 10:28:48 -0400

    feat: Add `Scene` class

---
+++ include/scene.h
@@ -0,0 +1,26 @@
+/*
+ * scene.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef SCENE_H
+#define SCENE_H
+
+class Scene
+{
+	public:
+		Scene();
+		virtual ~Scene();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* SCENE_H */ 
