b4f73ba8227d0168133d131d0d024a2cc59a4613
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-27 10:27:35 -0400

    feat: Add `Element` class

---
+++ include/element.h
@@ -0,0 +1,25 @@
+/*
+ * element.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+class Element
+{
+	public:
+		Element();
+			
+	private:
+		/* add your private declarations */
+};
+
+#endif /* ELEMENT_H */ 
---
+++ src/element.cc
@@ -0,0 +1,21 @@
+/*
+ * element.cc
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#include "element.h"
+
+
+Element::Element()
+{
+	
+}
+
+
