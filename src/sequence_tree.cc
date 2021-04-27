6802ef9e1f294af96e757840d5a4381328b43343
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-27 10:31:18 -0400

    feat: Add `SequenceTree` class

---
+++ src/sequence_tree.cc
@@ -0,0 +1,26 @@
+/*
+ * sequence_tree.cc
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#include "sequence_tree.h"
+
+
+SequenceTree::SequenceTree()
+{
+	
+}
+
+
+SequenceTree::~SequenceTree()
+{
+	
+}
+
